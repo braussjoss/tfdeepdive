959cfe659b6c0414d6aa3e61d523c4561dc25dda
braussjoss <brauliojossafat@hotmail.com>
2024-01-18 10:52:10 -0600

    Add thjrid subnet and tags III

--- variables.tf
+++ variables.tf
@@ -20,8 +20,8 @@ variable "environment" {
   default     = "development"
 }
 
-variable "billing_code"{
-  type = string
+variable "billing_code" {
+  type        = string
   description = "(Required) Billing code for network resources "
 }
 
